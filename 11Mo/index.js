c77060d8237c4528bc2cf61c9b217d755419b853
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-01-27 16:27:22 +0100

    javascript exercises

---
+++ 11Mo/index.js
@@ -0,0 +1,30 @@
+// const tax = 0.15;
+
+// let wage = prompt('Enter your hourly wage in euros');
+
+// let wageNumber = Number(wage);
+
+// let brutto = wageNumber * 8 * 21;
+// let netto = Math.round(brutto * (1 - tax));
+// console.log(netto);
+// // alert(netto);
+// document.write(netto);
+
+
+// alert(2020 - Number(prompt("what's your year of birth?")));
+
+// let mph = prompt("Enter miles per hour");
+// let kph = Math.round(mph * 1.609344);
+// alert(kph);
+
+// let crowns = prompt("Enter crowns");
+// let euros = Math.round(crowns / 25.695);
+// alert(euros);
+
+// let message = prompt("your message:");
+// document.write(Math.ceil(message.length/160));
+
+// let fahrnheit = Number(prompt("fahrnheits:").replace(',', '.'));
+// document.write((5/9)*(fahrnheit-32));
+
+document.write(Math.floor(Math.random()*6) + 1);
