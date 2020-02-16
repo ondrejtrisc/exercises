cd07ef9b14e526de209b8a741e0530937e6cf782
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-02-16 19:51:13 +0100

    fellowship exercise

--- 25Fr/fellowship/functions.php
+++ 25Fr/fellowship/functions.php
@@ -1,2 +1,42 @@
 <?php
+
+//6, 7, 8
+function leave_hobbiton($party) {
+  $party = array_merge($party, [
+    'merry' => 'Meriadoc Brandybuck',
+    'pippin' => 'Peregrin Took'
+  ]);
+  return $party;
+}
+
+//10, 11, 12
+function go_to_bree($party) {
+  $party['strider'] = 'Strider';
+  return $party;
+}
+
+//14, 15
+function go_to_weathertop($party) {
+  $party = array_merge($party, [
+    'Witch King of Angmar',
+    'Nazgûl #2',
+    'Nazgûl #3',
+    'Nazgûl #4',
+    'Nazgûl #5',
+    'Nazgûl #6',
+    'Nazgûl #7',
+    'Nazgûl #8',
+    'Nazgûl #9'
+  ]);
+  return $party;
+}
+
+//17, 18, 19
+function meet_arwen($party) {
+  $party['arwen'] = 'Arwen Undómiel';
+  array_splice($party, -10, 9);
+  return $party;
+}
+
+
 ?>
--- 25Fr/fellowship/journey.php
+++ 25Fr/fellowship/journey.php
@@ -1,9 +1,34 @@
 <?php
 
+//1
 $party = [
   'bilbo' => 'Bilbo Baggins',
   'frodo' => 'Frodo Baggins',
   'ring' => 'The One Ring'
 ];
+
+//2
+$party['gandalf'] = 'Gandalf the Grey';
+
+//3
+unset($party['bilbo']);
+
+//4
+$party['sam'] = 'Samwise Gamgee';
+
+//5
+unset($party['gandalf']);
+
+//9
+$party = leave_hobbiton($party);
+
+//13
+$party = go_to_bree($party);
+
+//16
+$party = go_to_weathertop($party);
+
+//20
+$party = meet_arwen($party);
 
 ?>
